ecd1a397167879b1c701d6672b1e9fd22bcc7448
warsaiqbal <160345806+warsaiqbal@users.noreply.github.com>
2024-03-30 07:36:01 -0700

    Add files via upload

---
+++ 9. Adding comments/index.ts
@@ -0,0 +1,9 @@
+//Hello world program
+console.log("Hello, World!");
+
+//Primitive data
+let myname = "ali" //string
+let age : number = 17 //number
+let ismarried : boolean = false //boolean
+let randomvalue : undefined = undefined //undefined, non-existing
+let newvalue : null = null //can be updated later
